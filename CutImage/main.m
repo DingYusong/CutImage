7ba617a7058b7441185a149b659d62b1fa0b464b
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-11-29 15:04:40 +0800

    Initial Commit

---
+++ CutImage/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CutImage
+//
+//  Created by 丁玉松 on 2018/11/29.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
