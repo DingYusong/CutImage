6ea6af7b757d1b4994f5e3003757c601c65b5c07
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-11-29 21:16:21 +0800

    clean

--- CutImage/main.m
+++ CutImage/main.m
@@ -7,11 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // 1.输入参数校验
+        if (argc < 2) {
+            NSLog(@"请输入图片地址");
+            return 0;
+        }
+        
+        NSString *inputFile = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        
+        NSString *outPutPath = [inputFile stringByDeletingPathExtension];
+        
+        CGFloat titleSize = 256;
+        
+        //加载大图
+        NSImage *image = [[NSImage alloc] initWithContentsOfFile:inputFile];
+        NSSize size = [image size];
+        NSArray<NSImageRep *> *representations = [image representations];
+        if ([representations count]) {
+            NSBitmapImageRep *representation = representations[0];
+            size.width = [representation pixelsWide];
+            size.height = [representation pixelsHigh];
+        }
+        
+        
+        
+        
     }
     return 0;
 }
